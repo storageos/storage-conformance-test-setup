7027d38e2f4ee6d963b5db1ce9a7fd30b9d8cd9d
Sunny <darkowlzz@protonmail.com>
2019-12-26 08:44:16 +0530

    Add linter

---
+++ lint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# shellcheck all the scripts. Ignore .git dir.
+find "$PWD" -path "$PWD/.git" -prune -o -type f -exec grep -Eq '^#!(.*/|.*env +)(sh|bash|ksh)' {} \; -print |
+  while IFS="" read -r file
+  do
+    shellcheck "$file"
+  done
