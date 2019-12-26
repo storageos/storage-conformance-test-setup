af88ffdbb8fa4ed2ef4850de03ddaf808488d72a
Sunny <darkowlzz@protonmail.com>
2019-12-26 09:05:49 +0530

    Fix travis config typo

--- lint.sh
+++ lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# shellcheck all the scripts. Ignore .git dir.
+# Check all the scripts. Ignore .git dir.
 find "$PWD" -path "$PWD/.git" -prune -o -type f -exec grep -Eq '^#!(.*/|.*env +)(sh|bash|ksh)' {} \; -print |
   while IFS="" read -r file
   do
